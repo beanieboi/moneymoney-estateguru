2c61c4a3b6eb51b1d9e01e9fb1e7b77b09db3fe1
ben <beanie@benle.de>
2017-12-05 14:02:25 +0700

    initial version

---
+++ estateguru.lua
@@ -0,0 +1,68 @@
+WebBanking{
+  version = 1.0,
+  url = "https://estateguru.co",
+  description = "Estateguru",
+  services= { "Estateguru" },
+}
+
+local currency = "EUR" -- fixme: Don't hardcode
+local currencyName = "EUR" -- fixme: Don't hardcode
+local connection
+local apiKey
+
+function SupportsBank (protocol, bankCode)
+  return protocol == ProtocolWebBanking and bankCode == "Estateguru"
+end
+
+function InitializeSession (protocol, bankCode, username, username2, password, username3)
+  connection = Connection()
+  content, charset, mimeType = connection:request("POST",
+  "https://estateguru.co/j_spring_security_check",
+  "j_username=" .. username .. "&j_password=" .. password,
+  "application/x-www-form-urlencoded; charset=UTF-8")
+
+  if string.match(connection:getBaseURL(), 'Sign In or Register') then
+      return LoginFailed
+  end
+end
+
+function ListAccounts (knownAccounts)
+  local account = {
+    name = "Estateguru",
+    accountNumber = "Estateguru",
+    currency = currency,
+    portfolio = true,
+    type = "AccountTypePortfolio"
+  }
+
+  return {account}
+end
+
+function RefreshAccount (account, since)
+  local s = {}
+  content = HTML(connection:get("https://estateguru.co/investment/main"))
+
+  available = content:xpath('/html/body/div[1]/div[2]/div[3]/div/h2/div/div/div/div[1]/h2'):text()
+  available = string.gsub(available, "€", "")
+
+  earnings = content:xpath('/html/body/div[1]/div[2]/div[3]/div/h2/div/div/div/div[2]/h2'):text()
+  earnings = string.gsub(earnings, "€", "")
+
+  invested = content:xpath('/html/body/div[1]/div[2]/div[3]/div/h2/div/div/div/div[3]/h2'):text()
+  invested = string.gsub(invested, "€", "")
+
+  local security = {
+    name = "Account Summary",
+    price = tonumber(invested + available),
+    purchasePrice = tonumber(invested + available - earnings),
+    quantity = 1,
+    curreny = nil,
+  }
+
+  table.insert(s, security)
+
+  return {securities = s}
+end
+
+function EndSession ()
+end
